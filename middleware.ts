d899b47c52cbd78dfc7cc9bfb59e015d3c8bca4f
novaksimic <novak.simic21@gmail.com>
2026-06-08 23:33:22 +0200

    small fix

--- middleware.ts
+++ middleware.ts
@@ -65,8 +65,9 @@
 // }
 
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export function middleware(req) {
+export function middleware(req: NextRequest) {
   console.log("Middleware started:", req.nextUrl.pathname);
 
   try {
